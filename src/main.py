a9a4efee05fe885b3d9d3f14d01393301658ade5
jdenozi <denozi.j@gmail.com>
2019-04-26 14:06:26 +0200

    Ajout argurment

--- src/main.py
+++ src/main.py
@@ -8,26 +8,25 @@ import os
 import argparse
 ObjectVcf=vcfobject.ObjectVcf
 ParseVcf=parsevcf.ParseVcf
-
+#sys.tracebacklimit=0
 ###############
 ###Arguments###
 ###############*
 
 parser = argparse.ArgumentParser(description='TE Gen Tool')
-parser.add_argument('-f','--file',
-                   help='File to process')
+parser.add_argument('-f','--file',help='File to process')
 parser.add_argument('-c','--clean', type=int, help='Remove nested TE, with values of nested' )
-parser.add_argument('file')
 
 args = parser.parse_args()
 if args.file:
 	list_objvcf=[]
 	for dir,subdir,files in os.walk(args.file):
 		[list_objvcf.append(ObjectVcf(0,dir,file)) for file in files]
-	for objvcf in list_objvcf:
-		objvcf.dataConvert()
-		if args.clean:
-			objvcf.removeNestedTE(1,args.clean)
-		if not args.clean:
-			objvcf.removeNestedTE(1)
+	if not list_objvcf:
+		raise FileNotFoundError("An error occurred with you file path")
+	if args.clean:
+		vcfobject.removeNestedTE(list_objvcf,args.clean)
+	if not args.clean:
+		vcfobject.removeNestedTE(list_objvcf)
+
 
